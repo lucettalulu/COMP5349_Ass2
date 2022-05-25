639abe220ce5055a5f9af7580993225a3b3509e5
lucettalulu <475345103@qq.com>
2022-05-25 23:47:23 +0800

    update

--- Ass2.py
+++ Ass2.py
@@ -48,8 +48,8 @@ spark = SparkSession \
     .getOrCreate()
 df = spark.read.format("json").load(text_file)
 df_data = df.select((explode("data").alias('data')))
-# df_data = df_data.select(explode("data.paragraphs").alias("paragraph"),'data.title')
-# df_data = df_data.select(explode("paragraph.qas").alias("qas"),"paragraph.context","title")
+df_data = df_data.select((explode("data.paragraphs").alias("paragraph")),'data.title')
+df_data = df_data.select((explode("paragraph.qas").alias("qas")),"paragraph.context","title")
 # contract_rdd = df_data.rdd
 contract_rdd = sc.parallelize(df.select("data").first()['data'])
 sample_rdd = contract_rdd.flatMap(flat_paragraph)
